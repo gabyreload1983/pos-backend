68e451f64dc877982170d7c78f3c56083ce5e3d6
Gabriel Godoy <gabyreload@gmail.com>
2025-07-11 14:28:06 -0300

    remove commnets

--- src/app.js
+++ src/app.js
@@ -66,7 +66,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-//TESTING
-// REMITO COMPLETO OK
-// REMITOS PARCIALES >> FALTA
